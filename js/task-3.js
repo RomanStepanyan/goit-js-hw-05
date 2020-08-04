d9187b4f300ca24b91a3e4bc01a3ec7cc8af5c49
RomanStepanyan <stepik10@gmail.com>
2020-08-04 13:31:34 +0300

    Revised tasks are added

--- js/task-3.js
+++ js/task-3.js
@@ -1,20 +1,17 @@
-const Storage = function (arr) {
-    this.items = arr; 
-}
-
-Storage.prototype.getItems = function() {
-    return this.items
-}
-
-Storage.prototype.addItem = function(itemName) {
-     this.items.push(itemName)
-}
-
-Storage.prototype.removeItem = function(itemName) {
-    this.items = this.items.filter(item => item !== itemName);
-}
-
-
+class Storage{
+    constructor(arr) {
+        this.items = arr;
+    }
+    getItems(){
+        return this.items;
+    }
+    addItem(itemName){
+        this.items.push(itemName);
+    }
+    removeItem(itemName){
+        this.items = this.items.filter(item => item !== itemName);
+    }
+} 
 const storage = new Storage([
     'Нанитоиды',
     'Пролонгер',
@@ -30,3 +27,39 @@ const storage = new Storage([
   
   storage.removeItem('Пролонгер');
   console.log(storage.items); // [ "Нанитоиды", "Железные жупи", "Антигравитатор", "Дроид" ]
+
+
+
+
+// const Storage = function (arr) {
+//     this.items = arr; 
+// }
+
+// Storage.prototype.getItems = function() {
+//     return this.items
+// }
+
+// Storage.prototype.addItem = function(itemName) {
+//      this.items.push(itemName)
+// }
+
+// Storage.prototype.removeItem = function(itemName) {
+//     this.items = this.items.filter(item => item !== itemName);
+// }
+
+
+// const storage = new Storage([
+//     'Нанитоиды',
+//     'Пролонгер',
+//     'Железные жупи',
+//     'Антигравитатор',
+//   ]);
+   
+//   const items = storage.getItems();
+//   console.log(items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор" ]
+  
+//   storage.addItem('Дроид');
+//   console.log(storage.items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор", "Дроид" ]
+  
+//   storage.removeItem('Пролонгер');
+//   console.log(storage.items); // [ "Нанитоиды", "Железные жупи", "Антигравитатор", "Дроид" ]
--- js/task-5.js
+++ js/task-5.js
@@ -1,15 +1,11 @@
 class Car {
-    constructor({speed = 0, price, maxSpeed, isOn = 'false', distance = 0}){
-        this.__speed = speed;
+    constructor({price, maxSpeed}){
+        this.__speed = 0;
         this.__price = price;
         this.__maxSpeed = maxSpeed;
-        this.__isOn = isOn;
-        this.__distance = distance;
-    }
-
-    static getSpecs(car) {
-        console.log(car);
-    }
+        this.__isOn = false;
+        this.__distance = 0;
+    }   
 
     set price (priceNew){
         return this.__price = priceNew;
@@ -38,6 +34,9 @@ class Car {
     drive(hours) {
        if (this.__isOn) return this.__distance +=this.__speed * hours;
     }
+    static getSpecs(car) {
+        console.log(car);
+    }
 }
 
 
@@ -60,3 +59,73 @@ Car.getSpecs(mustang);
 console.log(mustang.price); // 2000
 mustang.price = 4000;
 console.log(mustang.price); // 4000
+
+
+
+
+
+
+
+
+// class Car {
+//     constructor({speed = 0, price, maxSpeed, isOn = 'false', distance = 0}){
+//         this.__speed = speed;
+//         this.__price = price;
+//         this.__maxSpeed = maxSpeed;
+//         this.__isOn = isOn;
+//         this.__distance = distance;
+//     }
+
+//     static getSpecs(car) {
+//         console.log(car);
+//     }
+
+//     set price (priceNew){
+//         return this.__price = priceNew;
+//     }
+//     get price(){
+//         return this.__price
+//     }
+    
+//     turnOn() {
+//         return this.__isOn = true;
+//     }
+
+//     turnOff() {
+//         this.__isOn = false;
+//         this.__speed = 0;
+//     }
+
+//     accelerate(value) {
+//         return this.__speed < this.__maxSpeed ? this.__speed += value : this.__maxSpeed;
+//     }
+
+//     decelerate(value) {
+//         return this.__speed > 0 ? this.__speed -= value : 0;
+//     }
+
+//     drive(hours) {
+//        if (this.__isOn) return this.__distance +=this.__speed * hours;
+//     }
+// }
+
+
+// const mustang = new Car({ maxSpeed: 200, price: 2000 });
+
+// mustang.turnOn();
+// mustang.accelerate(50);
+// mustang.drive(2);
+
+// Car.getSpecs(mustang);
+// // maxSpeed: 200, speed: 50, isOn: true, distance: 100, price: 2000
+
+// mustang.decelerate(20);
+// mustang.drive(1);
+// mustang.turnOff();
+
+// Car.getSpecs(mustang);
+// // maxSpeed: 200, speed: 0, isOn: false, distance: 130, price: 2000
+
+// console.log(mustang.price); // 2000
+// mustang.price = 4000;
+// console.log(mustang.price); // 4000
